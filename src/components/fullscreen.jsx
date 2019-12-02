284321e41a487b75f6e10c260f23c83291ea572e
Luix70 <lortuno1@gmail.com>
2019-12-02 17:24:03 +0100

    Añadido web.config para compatibilidd IIS y react-router-dom

--- src/components/fullscreen.jsx
+++ src/components/fullscreen.jsx
@@ -8,7 +8,7 @@ const Fullscreen = props => {
   const defaultButtonStyle = "btn btn-light pl-4 d-block";
   return (
     <div className="col-12 fixed-top p-0 m-0 overflow-hidden max-vh-100 max-vw-100 vh-100 vw-100 overflow-hidden">
-      <div className="row bg-light">
+      <div className="row bg-light pt-5">
         <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl-2">
           <div
             className="btn-group btn-group-sm float-left iconOverlay"
