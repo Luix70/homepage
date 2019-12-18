cf601ef4cff803888e9b8753172627125e8d7fc8
Luix70 <lortuno1@gmail.com>
2019-12-18 18:13:20 +0100

    traducida col eco

--- src/components/articulo.jsx
+++ src/components/articulo.jsx
@@ -6,17 +6,17 @@ const Articulo = prop => {
       <div className="col-3 col-md-2 col-lg-1 bg-light d-flex align-self-stretch align-items-center px-0 text-muted">
         <h4 className="text-center w-100 text-monospace">{art.cod}</h4>
       </div>
-      <div className="col-9 col-md-10 col-lg-3 bg-light align-self-stretch my-auto pr-0 ">
+      <div className="col-9 col-md-10 col-lg-4 bg-light align-self-stretch my-auto pr-0 ">
         <h4 className="text-start text-info">
           {art.desc[lan].split("#").map(line => (
-            <small key={line} className="text-nowrap">
+            <small key={line} className="">
               {line}
               {BSBreak === "md" ? null : <br />}
             </small>
           ))}
         </h4>
       </div>
-      <div className="col-12 col-sm-10 col-md-9 col-lg-8 bg-light mx-auto align-self-stretch align-items-center pr-5">
+      <div className="col-12 col-sm-10 col-md-9 col-lg-7 bg-light mx-auto align-self-stretch align-items-center pr-5">
         <div className="row d-flex justify-content-around">
           {art.imagenes
             ? art.imagenes.map(img => (
