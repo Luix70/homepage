df31ac07deb01881a2da212b6f3928e76b0fd5d7
Luix70 <lortuno1@gmail.com>
2020-01-23 12:20:43 +0100

    responsiveness operations

--- src/components/linea.jsx
+++ src/components/linea.jsx
@@ -5,39 +5,41 @@ const Linea = ({ linea, isNew, lan }) => {
     <React.Fragment>
       {isNew ? (
         <React.Fragment>
-          <div className="pb-1 mt-3 font-weight-bold border-bottom text-primary border-primary">
-            {"P-" +
-              linea.pedido +
-              ". (" +
-              linea.fechapedido +
-              "). " +
-              t.RE[lan] +
-              linea.referencia}
+          <div className="row pb-1 px-2 mt-3  border-bottom border-top  text-primary border-light">
+            <div className="col-12 col-md-3 font-weight-bold px-1">
+              {"P-" + linea.pedido + ". (" + linea.fechapedido + ") "}
+            </div>
+            <div className="col-12 col-md-9 font-italic pl-4">
+              {t.RE[lan] + linea.referencia}
+            </div>
           </div>
         </React.Fragment>
       ) : null}
-      <div className="row">
-        <div className="col-1">
+      <div className="row px-2">
+        <div className="col-2 col-md-1 p-0 pl-2">
           <strong>{linea.coart}</strong>
         </div>
-        <div className="col-9">{linea.descripcion}</div>
+        <div className="col-7 col-md-9 p-0">{linea.descripcion}</div>
         <div
           className={
             linea.cantidad === 1 || linea.cantidad === 0
-              ? "col-2 text-right"
-              : "col-2 text-right text-danger"
+              ? "col-3 col-md-2 p-0 pr-2 text-right"
+              : "col-3 col-md-2 p-0 pr-2 text-right text-danger"
           }
         >
           {linea.cantidad === 1 ? "" : linea.cantidad + " x "}
-          {linea.precio !== 0
-            ? Number.parseFloat(linea.precio).toFixed(2) + " €"
-            : ""}
+          <strong>
+            {linea.precio !== 0
+              ? Number.parseFloat(linea.precio).toFixed(2) + " €"
+              : ""}
+          </strong>
         </div>
       </div>
-      <div className="row">
-        <div className="col-1">&nbsp;</div>
-        <div className="col-9 text-muted pl-4">{linea.ref_linea}</div>
-        <div className="col-1">&nbsp;</div>
+      <div className="row mb-2">
+        <div className="col-2 col-md-1 p-0">&nbsp;</div>
+        <div className="col-7 col-md-9 p-0 pl-2 text-muted  font-italic">
+          {linea.ref_linea}
+        </div>
       </div>
     </React.Fragment>
   );
