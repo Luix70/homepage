c6ad1ad3f76090d59a91341946f2a644c2053a4f
Luix70 <lortuno1@gmail.com>
2020-02-12 17:27:18 +0100

    panel buscador expandible

--- src/components/buscador.jsx
+++ src/components/buscador.jsx
@@ -9,22 +9,22 @@ class Buscador extends Component {
   render() {
     const { expandido } = this.state;
     return (
-      <div className="row d-flex justify-content-around">
-        <div
-          className="d-flex align-items-center p-0 m-0"
-          onClick={this.onExpand}
-        >
-          <MaterialIcon
-            icon={expandido ? "expand_less" : "expand_more"}
-            size={36}
-          ></MaterialIcon>{" "}
-          Búsqueda
-          <MaterialIcon
-            icon={expandido ? "expand_less" : "expand_more"}
-            size={36}
-          ></MaterialIcon>
+      <div className="row">
+        <div className="col-12">
+          <div
+            className="d-flex align-items-center p-0 m-0"
+            onClick={this.onExpand}
+          >
+            <span className="ml-3">Búsqueda</span>
+            <MaterialIcon
+              icon={expandido ? "expand_less" : "expand_more"}
+              size={36}
+            ></MaterialIcon>
+          </div>
         </div>
-        <div className={"row bg-info p-5" + (expandido ? "d-block" : "d-none")}>
+        <div
+          className={"col-12 bg-info p-5 " + (expandido ? "d-block" : "d-none")}
+        >
           &nbsp;
         </div>
       </div>
