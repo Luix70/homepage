b9d76b83f3d54f3664938fab223cb4f8f4f6ddb9
Luix70 <lortuno1@gmail.com>
2020-03-11 15:49:44 +0100

    lanRedirect

--- src/components/lanRedirect.jsx
+++ src/components/lanRedirect.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-
 import { Redirect } from "react-router-dom";
+
 const LanRedirect = props => {
   const { handleLanguage, lan } = props;
-  console.log(handleLanguage, lan);
+  //console.log(handleLanguage, lan);
   handleLanguage(lan);
   return <Redirect to="/"></Redirect>;
 };
