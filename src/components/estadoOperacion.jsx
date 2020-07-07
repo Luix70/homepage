053712fe75f9ac61482cf3f74c6ca86fbc05b2bb
Luix70 <lortuno1@gmail.com>
2020-07-07 18:29:00 +0200

    filtrodocs

--- src/components/estadoOperacion.jsx
+++ src/components/estadoOperacion.jsx
@@ -202,18 +202,16 @@ const EstadoOperacion = (props) => {
                 </div>
               </div>
               <div className="col-12 col-md-6 bg-lighter-gray p-2 pl-5 border-bottom">
-                {estado.docs.length > 0 &&
-                (usuario.TipoEntidad !== "CL" ||
-                  estado.estado === "CO" ||
-                  estado.estado === "FA") ? (
-                  <span>{t[estado.docs[0].codTipo][lan]}</span>
-                ) : null}
+                {estado.docs.length > 0
+                  ? null //<span>{t[estado.docs[0].codTipo][lan]}</span>
+                  : null}
                 {estado.docs.map((scan) => {
                   return (
                     <DocView
                       estado={estado.estado}
                       key={scan.numerador}
                       tipo={scan.TipoImagen}
+                      codTipo={scan.codTipo}
                       ruta={scan.ruta + "\\" + scan.documento}
                       onClick={handleClick}
                       td={scan.tipodoc}
