d7d18184f8e0b4d133e5647fab72beee65aa5294
Luix70 <lortuno1@gmail.com>
2020-08-11 17:28:34 +0200

    Stop messing with bootstrap css

--- src/components/portal.jsx
+++ src/components/portal.jsx
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
+import t from "./portal.lit.json";
 class Portal extends Component {
   state = {};
   render() {
+    const { lan } = this.props;
     return (
-      <div className="container">
-        <h1>Portal Cliente</h1>
+      <div className="d-flex mt-5 ">
+        <div className="row m-0 p-0 w-100 justify-content-around ">
+          <div className="col-11 col-sm-8 col-md-6 col-xl-4 m-0 px-2">
+            <h1 className="text-center">{t.TI[lan]}</h1>
+            <hr />
+          </div>
+        </div>
       </div>
     );
   }
