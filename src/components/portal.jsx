54819ee446c025c6a1e3ce8a683ea22f5272ad3b
Luix70 <lortuno1@gmail.com>
2020-08-11 18:45:58 +0200

    layout portal (unfinished)

--- src/components/portal.jsx
+++ src/components/portal.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { Component } from "react";
 import t from "./portal.lit.json";
 class Portal extends Component {
@@ -10,6 +11,78 @@ class Portal extends Component {
           <div className="col-11 col-sm-8 col-md-6 col-xl-4 m-0 px-2">
             <h1 className="text-center">{t.TI[lan]}</h1>
             <hr />
+          </div>
+          <div className="row mt-3 mx-4">
+            <div className="col-sm-12 col-md-6 col-lg-3 col-xl-3">
+              <div className="card text-center p-4 h-100">
+                <img
+                  src="/resources/img/Customer.png"
+                  className="card-img-top img-fluid h-50"
+                  alt=""
+                />
+                <div className="card-block">
+                  <h3 className="card-title text-center">Ficha Cliente</h3>
+
+                  <p className="card-text text-center">
+                    Datos de Facturación, representante, cambio de contraseña..
+                    etc
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-sm-12 col-md-6 col-lg-3 col-xl-3">
+              <div className="card text-center p-4 h-100">
+                <img
+                  src="/resources/img/Documents.png"
+                  className="card-img-top img-fluid h-50"
+                  alt=""
+                />
+                <div className="card-block">
+                  <h3 className="card-title text-center">Operaciones</h3>
+
+                  <p className="card-text text-center ">
+                    Datos de Facturación, representante, cambio de contraseña..
+                    etc
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-sm-12 col-md-6 col-lg-3 col-xl-3">
+              <div className="card text-center p-4 h-100">
+                <img
+                  src="/resources/img/Ventas.png"
+                  className="card-img-top img-fluid h-50"
+                  alt=""
+                />
+                <div className="card-block">
+                  <h3 className="card-title text-center">Ofertas Flash</h3>
+
+                  <p className="card-text text-center">
+                    Datos de Facturación, representante, cambio de contraseña..
+                    etc
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-sm-12 col-md-6 col-lg-3 col-xl-3">
+              <div className="card text-center p-4 h-100">
+                <img
+                  src="/resources/img/catalog.png"
+                  className="card-img-top img-fluid h-50"
+                  alt=""
+                />
+                <div className="card-block">
+                  <h3 className="card-title text-center">Documentacion</h3>
+
+                  <p className="card-text text-center">
+                    Datos de Facturación, representante, cambio de contraseña..
+                    etc
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
