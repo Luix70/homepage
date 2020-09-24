544a5c0a1dd4d768372651d5bc89b9506ed9a94f
Luix70 <lortuno1@gmail.com>
2020-09-24 12:26:02 +0200

    formulario datos Cleinte

--- src/components/clienteCarrito.jsx
+++ src/components/clienteCarrito.jsx
@@ -5,96 +5,93 @@ class ClienteCarrito extends Component {
 
   render() {
     const { datosCliente } = this.props;
-
+    console.log(datosCliente);
     return (
       <div className="bg-light row ">
         <div className="col-12 bg-info text-center ">
           <h1>Datos Cliente</h1>
         </div>
-
-        <div className="d-flex justify-content-center col-12 pt-4">
-          <div className="col-9">
-            <form>
-              <div className="form-row">
-                <div className="form-group row  small">
+        <form className="w-100 py-4 px-4 mx-4">
+          <div className="row ">
+            <div className="col-12 col-md-12 col-lg-3">
+              <div className="row mb-2">
+                <div className="col-12 col-sm-3 ">
                   <label
-                    className="col-sm-2 col-form-label "
-                    htmlFor="inputEmail4"
+                    className="font-weight-bold m-0 pt-2"
+                    htmlFor="CodCliente"
                   >
-                    CIF
+                    Cod
                   </label>
-                  <div class="col-sm-10">
-                    <input
-                      type="email"
-                      className="form-control-plaintext"
-                      id="inputEmail4"
-                      value={datosCliente.Cif}
-                    />
-                  </div>
                 </div>
-                <div className="form-group col-md-6">
-                  <label htmlFor="inputPassword4">Password</label>
+                <div className="col-12 col-sm-9">
                   <input
-                    type="password"
-                    className="form-control"
-                    id="inputPassword4"
-                    placeholder="Password"
+                    type="text"
+                    className="form-control-plaintext"
+                    id="CodCliente"
+                    placeholder={datosCliente.CodCliente}
                   />
                 </div>
               </div>
-              <div className="form-group">
-                <label htmlFor="inputAddress">Address</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="inputAddress"
-                  placeholder="1234 Main St"
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="inputAddress2">Address 2</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="inputAddress2"
-                  placeholder="Apartment, studio, or floor"
-                />
-              </div>
-              <div className="form-row">
-                <div className="form-group col-md-6">
-                  <label htmlFor="inputCity">City</label>
-                  <input type="text" className="form-control" id="inputCity" />
-                </div>
-                <div className="form-group col-md-4">
-                  <label htmlFor="inputState">State</label>
-                  <select id="inputState" className="form-control">
-                    <option selected>Choose...</option>
-                    <option>...</option>
-                  </select>
-                </div>
-                <div className="form-group col-md-2">
-                  <label htmlFor="inputZip">Zip</label>
-                  <input type="text" className="form-control" id="inputZip" />
-                </div>
-              </div>
-              <div className="form-group">
-                <div className="form-check">
-                  <input
-                    className="form-check-input"
-                    type="checkbox"
-                    id="gridCheck"
-                  />
-                  <label className="form-check-label" htmlFor="gridCheck">
-                    Check me out
+            </div>
+            <div className="col-12 col-md-12 col-lg-9">
+              <div className="row mb-2">
+                <div className="col-12 col-sm-3">
+                  <label className="font-weight-bold m-0 pt-2" htmlFor="Rzs">
+                    R. Social
                   </label>
                 </div>
+                <div className="col-12 col-sm-9">
+                  <input
+                    type="text"
+                    className="form-control-plaintext"
+                    id="Rzs"
+                    placeholder={datosCliente.Rzs}
+                  />
+                </div>
               </div>
-              <button type="submit" className="btn btn-primary">
-                Sign in
-              </button>
-            </form>
+            </div>
           </div>
-        </div>
+
+          <div className="row ">
+            <div className="col-12 col-md-12 col-lg-3">
+              <div className="row mb-2">
+                <div className="col-12 col-sm-3 ">
+                  <label className="font-weight-bold m-0 pt-2" htmlFor="Cif">
+                    Cif
+                  </label>
+                </div>
+                <div className="col-12 col-sm-9">
+                  <input
+                    type="text"
+                    className="form-control-plaintext"
+                    id="Cif"
+                    placeholder={datosCliente.Cif}
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="col-12 col-md-12 col-lg-9">
+              <div className="row mb-2">
+                <div className="col-12 col-sm-3">
+                  <label
+                    className="font-weight-bold m-0 pt-2"
+                    htmlFor="NombreComercial"
+                  >
+                    N. Comercial
+                  </label>
+                </div>
+                <div className="col-12 col-sm-9">
+                  <input
+                    type="text"
+                    className="form-control-plaintext"
+                    id="NombreComercial"
+                    placeholder={datosCliente.Nombrecomercial}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </form>
       </div>
     );
   }
