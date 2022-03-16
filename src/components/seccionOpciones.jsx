4febce6b59ca40fa818b51e0a29c2f7c57b2032e
Luix70 <lortuno1@gmail.com>
2022-03-16 16:39:22 +0100

    tweak bootstrap

--- src/components/seccionOpciones.jsx
+++ src/components/seccionOpciones.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-const SeccionOpciones = props => {
+const SeccionOpciones = (props) => {
   const { seccion, lan } = props;
 
   //console.log(seccion, lan);
@@ -7,7 +7,7 @@ const SeccionOpciones = props => {
     <div className="col-12">
       <h3 className="text-center mb-4">{seccion.titulo[lan].toUpperCase()}</h3>
       <div className="row d-flex justify-content-center align-items-start">
-        {seccion.subopciones.map(so => (
+        {seccion.subopciones.map((so) => (
           <div
             key={seccion.subopciones.indexOf(so)}
             className="col-12 col-sm-6 col-lg-4   align-content-start"
@@ -16,9 +16,12 @@ const SeccionOpciones = props => {
 
             <ul className="list-group">
               {so.subtitulo[lan] ? (
-                <li className="list-group-item p-0 m-0" key="-1">
+                <li
+                  className="list-group-item p-0 m-2 border-top-0 border-right-0 border-left-0"
+                  key="-1"
+                >
                   <h6 className="text-small text-small bg-light m-0 p-0">
-                    {so.subtitulo[lan].split("#").map(line => (
+                    {so.subtitulo[lan].split("#").map((line) => (
                       <span key={line}>
                         {line}
                         <br />
@@ -27,7 +30,7 @@ const SeccionOpciones = props => {
                   </h6>
                 </li>
               ) : null}
-              {so.opciones.map(op => (
+              {so.opciones.map((op) => (
                 <li className="list-group-item" key={so.opciones.indexOf(op)}>
                   {op[lan]}
                 </li>
