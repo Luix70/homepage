ecfe67937c177b8184daf6a84e57ede941dc54d7
Luix70 <lortuno1@gmail.com>
2024-02-22 07:13:26 +0100

    cambios en el tamaño de los componentes

--- src/components/heroSection.jsx
+++ src/components/heroSection.jsx
@@ -4,14 +4,14 @@ const HeroSection = props => {
     const {windowWidth, windowHeight } = props;
     return (
       <div id="heroSection" className="bg-white w-100 ">
-        <div className="mx-lg-5 mx-md-3 py-3 ">
+        <div className="mx-lg-5 mx-md-3  ">
             <div className="sectionContainer bg-white min-vh-50 ">
               <div className="heroContainer " style={{backgroundImage: "url(/resources/img/backgrounds/bg1_tn_" + WhichBotstrapBreak(windowWidth, windowHeight) + ".jpg)" }}>
                  <div className="heroOverlay w-100 h-100">
                   <div id="textos" className="col-12 col-md-6 p-4 p-md-4 text-light h-100 ">
                     <img src='/resources/img/logoIndesan_animated.svg' alt="" className="h-33"/>
-                    <h2 className="card-title h1 my-4">El referente de calidad en mesas de comedor</h2>
-                    <p className="card-text h4 my-4 font-weight-light font-italic ">Descubre nuestras colecciones de mesas de comedor modernas, construidas con materiales naturales como maderas, cerámicas y estructuras de acero.</p>
+                    <h2 className="card-title h1 my-4 text-light">El referente de calidad en mesas de comedor</h2>
+                    <p className="card-text h5 my-4 font-weight-lighter font-italic">Descubre nuestras colecciones de mesas de comedor modernas, construidas con materiales naturales como maderas, cerámicas y estructuras de acero.</p>
                     <a href="/" class="btn btn-primary btn-lg " role="button" aria-disabled="false">Conócenos</a>
                   </div>
                   </div>
--- src/components/materialsSection.jsx
+++ src/components/materialsSection.jsx
@@ -2,10 +2,10 @@ import React from "react";
 const MaterialsSection = prop => {
     //const { art, lan, folder, BSBreak } = prop;
     return (
-      <div id="materialsSection" className="bg-success w-100 ">
+      <div id="materialsSection" className="bg-white w-100 ">
           <div className="mx-lg-5 mx-md-3 py-3">
           <div className="sectionContainer bg-white">
-                MaterialsSection
+                
           </div>
           
         </div>
