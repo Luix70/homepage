5ff617f77924fc8976d4dd5dbfc0a9f8e991f65b
Luix70 <lortuno1@gmail.com>
2024-02-23 12:44:57 +0100

    cambios en el resizing de las imagenes

--- src/components/qualitySection.jsx
+++ src/components/qualitySection.jsx
@@ -5,7 +5,7 @@ const QualitySection = props => {
     const {windowWidth, windowHeight ,lan} = props;
     //const { art, lan, folder, BSBreak } = prop;
     return (
-      <div id="qualitySection" className="bg-secondary w-100 mt-0 mt-md-3 ">
+      <div id="qualitySection" className="bg-white w-100 mt-0 mt-md-3 ">
           <div className="mx-lg-5 mx-md-3  ">
             <div className="sectionContainer bg-white min-vh-50  ">
                 <div className="qualityContainer  text-center  mx-0 h-100 ">
@@ -14,21 +14,17 @@ const QualitySection = props => {
                              <p className="ParagraphQuality text-secondary">{t.PA[lan]}</p>
                     </div>
         
-                    <div className="m-0 p-0 h-100 bg-success d-md-flex mt-2 h-100">
+                    <div className="m-0 p-0 h-100 bg-success d-md-flex mt-2 ">
 
 
 
-                        <div className="divImagen bg-white col-12 order-md-2 col-md-6">
-                            
-                        
-                                <img src="/resources/img/calidad/Calidad1.jpg"  className="w-100 imagenCalidad" alt="" />
-                               
-                            
-
+                        <div className="divImagen bg-white col-12 order-md-2 col-md-6 ">
+                          
+                               <img src="/resources/img/calidad/Calidad1.jpg"  className="w-100 imagenCalidad" alt="" />
                         </div>
 
                         <div className="divTextos col-12 bg-light order-md-1 col-md-6">
-                            
+                        <p className="ParagraphQuality text-secondary">{t.PA[lan]}</p>
 
                         </div>
                             
